01e90489ea90e519454c163e68b6334800456b58
maheshbansod <coolkevin011@gmail.com>
2020-05-23 22:51:10 +0530

    both players can move turn by turn now

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 
 class Game {
-    constructor(ctx, size = 10, board=null) {
+    constructor(ctx, size = 10, piececolors=['black','red'], board=null) {
         this.ctx = ctx;
+        this.piececolors = piececolors;
 
         //making it square -> chooses the smaller side as side of square
         if(this.ctx.canvas.height < this.ctx.canvas.width)
@@ -23,21 +24,58 @@ class Game {
             this.board = new  Array(this.size).fill(0).map(()=>(new Array(this.size).fill(0)));
             for(var i=0;i<3;i++) {
                 for(var j=i%2;j<this.size;j+=2) {
-                    this.board[i][j]=1; //black
+                    this.board[i][j]=1; //piece that start above
                 }
                 for(var j=(i+1)%2;j<this.size;j+=2) {
-                    this.board[this.size-i-1][j]=2; //red
+                    this.board[this.size-i-1][j]=2; //pieces that start below
                 }
             }
         }
 
-        this.turn = 2; //red plays first.
+        this.turn = 2;
+        this.total_moves = 0;
     }
 
-    play() {
+    /*play() {
+        maybe make it using a game loop or sthing in the future?
         this.drawBoard();
+    }*/
 
+    /**
+     * 
+     * @brief Move piece to position fpos (if possible)
+     * 
+     * @param {Object {i,j}} piece Coordinate of a piece that's to be moved
+     * @param {Object {i,j}} fpos Coordinate of the final position of piece
+     * 
+     * @returns
+     */
+    makeJump(piece, fpos) {
+        this.board[fpos.i][fpos.j] = this.turn; //piece moved
+        this.board[piece.i][piece.j] = 0; //set position empty
+    }
+
+    /**
+     * @brief checks if piece can be moved to fpos
+     * 
+     * @param {Object {i,j}} piece Coordinate of a piece that's to be moved
+     * @param {Object {i,j}} fpos Coordinate of the final position of piece
+     * 
+     * @returns true if piece can jump to fpos, false otherwise
+     */
+    isJumpabble(piece, fpos) {
+        if(this.turn != this.board[piece.i][piece.j]) //it's not piece's turn
+            return false;
+        if(this.board[fpos.i][fpos.j] != 0) //final position is not empty
+            return false;
         
+        if(piece.j == fpos.j-1 || piece.j == fpos.j+1)
+            if(( this.turn == 2 && piece.i == fpos.i+1)||(this.turn == 1 && piece.i == fpos.i-1)) {
+
+                return true; //only return true if fpos is valid for the piece
+            }
+        
+        return false;
     }
 
     handleClick(x, y) {
@@ -52,10 +90,25 @@ class Game {
                 this.selected = {i:i, j:j};
                 this.drawBoard();
             }
-        } else {//clicked on blank space - so either (TODO:make a move) or deselect.
+        } else { //clicked on blank space - so either (TODO:make a move) or deselect.
+
+            if(this.selected) {
+                //TODO: check if move is capture or jump and take appropriate action
+
+                if(this.isJumpabble(this.selected,{i,j})) {
+                    this.makeJump(this.selected, {i,j})
+                    this.nextTurn();
+                }
+            }
+
             this.selected = null;
             this.drawBoard();
         }
+    }
+
+    nextTurn() {
+        this.turn = (this.turn == 1)?2:1;
+        this.total_moves++;
     }
 
     resizeCanvas(width, height) {
@@ -86,8 +139,8 @@ class Game {
 
                 //draw piece
                 if(this.board[i][j] != 0) {
-                    if(this.board[i][j]==1) ctx.fillStyle = '#000'; //black
-                    else if(this.board[i][j]==2) ctx.fillStyle = '#f00'; //red
+                    if(this.board[i][j]==1 || this.board[i][j]==2)
+                        ctx.fillStyle = this.piececolors[this.board[i][j]-1];
 
                     ctx.beginPath();
                     ctx.arc( (2*j+1)*ts/2, (2*i+1)*ts/2, ts*2/5, 0, 2*Math.PI);
