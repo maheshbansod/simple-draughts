7edd41a22f7f89dafd6463c72b00079ec07fc2f5
maheshbansod <coolkevin011@gmail.com>
2020-05-25 14:38:28 +0530

    shows possible moves(excluding capture)

--- game.js
+++ game.js
@@ -42,6 +42,49 @@ class Game {
     }*/
 
     /**
+     * @brief checks if a coordinate is valid(inside the board)
+     * 
+     * @param {Object {i,j}} pos Position to check
+     * 
+     * @returns true if coords are inside the board, false otherwise.
+     */
+    isInside(pos) {
+        return (pos.i < this.board.length && pos.i >= 0 && pos.j >= 0 && pos.j < this.board[0].length);
+    }
+
+    /**
+     * @brief Find possible moves for a piece . ONLY RETURNS JUMPS . TODO: CAPTURE MOVES
+     * 
+     * @param {Object {i,j}} piece Piece
+     * 
+     * @returns array containing the possible moves.
+     */
+    findPossibleMovesFor(piece) {
+        var i=piece.i, j=piece.j;
+        var adjs=[];
+        adjs.push({j:j-1},{j:j+1})
+        if(this.turn == 1) {//the pieces that go from top to bottom
+            adjs[0].i = adjs[1].i = i+1;
+        } else adjs[0].i = adjs[1].i = i-1;
+        
+        var moves = [];
+        for(var it=0;it<2;it++) {
+            if(!this.isInside(adjs[it]))
+                continue;
+            var p = this.board[adjs[it].i][adjs[it].j];
+            if(p == 0) { // empty
+                moves.push({type:'jump',move:adjs[it]})
+            } else if(this.turn == p) continue; //same player's piece
+            else { //capture time maybe
+                //var captree = getCaptureTree(piece);
+                //captree
+            }
+        }
+
+        return moves;
+    }
+
+    /**
      * 
      * @brief Move piece to position fpos (if possible)
      * 
@@ -97,7 +140,19 @@ class Game {
             //set `selected` based on turn
             if(this.turn == this.board[i][j]) {
                 this.selected = {i:i, j:j};
+                var possible = this.findPossibleMovesFor(this.selected);
+                possible.forEach( (elem)=> {
+                    if(elem.type=='jump')
+                        this.board[elem.move.i][elem.move.j]=-12; //mark possible
+                    else if(elem.type == 'capture') {
+                        ;
+                    }
+                });
                 this.drawBoard();
+                possible.forEach( (elem)=> {
+                    if(elem.type=='jump')
+                        this.board[elem.move.i][elem.move.j]=0; //mark empty again.
+                }); 
             }
         } else { //clicked on blank space - so either (TODO:make a move) or deselect.
 
@@ -147,13 +202,16 @@ class Game {
                 ctx.fillRect(j*ts, i*ts, ts, ts);
 
                 //draw piece
-                if(this.board[i][j] != 0) {
-                    if(this.board[i][j]==1 || this.board[i][j]==2)
-                        ctx.fillStyle = this.piececolors[this.board[i][j]-1];
+                if(this.board[i][j]==1 || this.board[i][j]==2) {
+                    ctx.fillStyle = this.piececolors[this.board[i][j]-1];
 
                     ctx.beginPath();
                     ctx.arc( (2*j+1)*ts/2, (2*i+1)*ts/2, ts*2/5, 0, 2*Math.PI);
                     ctx.fill();
+                } else if(this.selected && this.board[i][j] == -12) {
+                    ctx.fillStyle = 'green';
+
+                    ctx.fillRect(j*ts+ts/10, i*ts+ts/10, ts*4/5, ts*4/5);
                 }
             }
         }
