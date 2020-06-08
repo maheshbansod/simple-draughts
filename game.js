814e27ef54afd33a3f98d04ce0af2d3b0a9ff85e
maheshbansod <coolkevin011@gmail.com>
2020-06-08 17:04:27 +0530

    implemented minimax to find best move

--- game.js
+++ game.js
@@ -50,28 +50,149 @@ class Game {
         this.mandatory_capture = true;
         this.showPossibleMoves = true;
 
+        this.minmax_depth_limit = 5;
+
         this.crownimg = document.createElement('img')
         this.crownimg.src = 'crown.png';
     }
 
+    minmaxscore(move, player, mboard=this.board, depth=1) {
+        var board = mboard; //trying without copying
+        //mboard.map( (row)=>Array.from(row) ); //copy board
+
+        var undoinfo = this.doMove(move, board);
+        var score = 0;
+
+        var mult;
+        if(player == 1) mult=-1;
+        else mult = 1;
+
+        if(this.hasWonOnBoard(player, board)) {
+            score = 10;
+        } else if(this.hasLostonBoard(player,board)) {
+            score =  -10;
+        } else if(depth==this.minmax_depth_limit) {
+            score = this.intermediateScore(player, board);
+        } else {
+
+            var opponent = (player==1)?2:1;
+
+            var possibleMoves = this.findPossibleMoves(opponent, board);
+
+            var totalscore = 0;
+            possibleMoves.forEach( (move)=> {
+                totalscore += this.minmaxscore(move, opponent, board,depth+1);
+            });
+            score = totalscore;
+        }
+
+        this.undoMove(move, board, undoinfo.deadpieces);
+        if(undoinfo.promoted) board[move.piece.i][move.piece.j]-=2; //demote if promoted
+
+        return mult*score;
+    }
+
     /**
-     * This function will play one turn of the player whose turn it is.
+     * @brief just calculates how many more pieces `player` has
      */
-    /*playTurn() {
-        var board = copy present board; //or maybe do and undo moves on the actual board?
-        var possibleMoves = this.findPossibleMoves(board, player);
-        for (move in possibleMoves) {
-            this.doMove(move, board);
+    intermediateScore(player, board=this.board) {
+        var mult=(player==1)?-1:1;
+        var counts = [0,0,0,0];
+        for(var i=0;i<this.size;i++)
+            for(var j=0;j<this.size;j++)
+                if(board[i][j]!=0)
+                    counts[board[i][j]-1]++;
+        //console.log(counts);
+        return mult*(counts[1]+counts[3]*2 - counts[0] -counts[2]*2);
+    }
+
+    /**
+     * This function will find the best move for `player` based on the current board state.
+     */
+    findBestMove(player=this.turn) {
+        
+        var possibleMoves = this.findPossibleMoves(player);
+        if(!possibleMoves) return;
+
+        var maxscore = 0, bestmove = possibleMoves[0];
+        possibleMoves.forEach( (move) => {
             
-            movescore = this.minmaxscore(board, player);
+            var movescore = this.minmaxscore(move, player);
+
             if(maxscore < movescore) {
                 bestmove = move;
                 maxscore = movescore;
             }
-        }
+        });
 
-        this.doMove(bestmove, this.board);
-    }*/
+        return bestmove;
+    }
+
+    doMove(move, board=this.board) {
+        var p = move.piece;
+        if(move.type=='jump') {
+            return this.makeJump(move.piece, move.move, board);
+        } else if(move.type=='capture') {
+            return this.doCapture(move.piece, move.moves, board);
+        }
+    }
+
+    undoMove(move, board=this.board,  deadpieces=null) {
+        if(move.type == 'jump') {
+            this.undoJump(move.piece, move.move, board);
+        } else if(move.type=='capture') {
+            this.undoCapture(move.piece, move.moves, board, deadpieces)
+        }
+    }
+
+    /**
+     * @brief undoes a jump
+     * 
+     * @param {i,j} from Position the piece will return to
+     * @param {i,j} fpos Position the piece is at before undo
+     * @param {Array} board 
+     */
+    undoJump(from, fpos, board=this.board) {
+        var pp = board[fpos.i][fpos.j];
+        board[from.i][from.j] = pp;
+        board[fpos.i][fpos.j]=0;
+    }
+
+    undoCapture(from, moves, board=this.boad, deadpieces=null) {
+        if(!deadpieces)
+            return;
+        moves.forEach((el,i)=>{
+            board[el.tokill.i][el.tokill.j]=deadpieces[i];//resurrect
+        });
+        //then jump back
+        var jumpat = moves[moves.length-1].jumpat;
+        var pp = board[jumpat.i][jumpat.j]; //the piece
+        board[from.i][from.j] = pp;
+        board[jumpat.i][jumpat.j] = 0;
+    }
+
+    hasWonOnBoard(player, board=this.board) {
+        var cp = 0; //opponent player pieces count
+
+        var opponent = (player==1)?2:1;
+        for(var i=0;i<this.size;i++)
+            for(var j=0;j<this.size;j++)
+                if(board[i][j]!=0 && board[i][j]%2==opponent%2)
+                    cp++;
+        if(cp == 0)
+            return true;
+    }
+
+    hasLostonBoard(player, board=this.board) {
+        var cp = 0; //player pieces count;
+
+        for(var i=0;i<this.size;i++)
+            for(var j=0;j<this.size;j++)
+                if(board[i][j]!=0 && board[i][j]%2==player%2)
+                    cp++;
+        if(cp == 0)
+            return true;
+    }
 
     /**
      * @brief checks if a coordinate is valid(inside the board)
@@ -80,25 +201,8 @@ class Game {
      * 
      * @returns true if coords are inside the board, false otherwise.
      */
-    isInside(pos) {
-        return (pos.i < this.board.length && pos.i >= 0 && pos.j >= 0 && pos.j < this.board[0].length);
-    }
-
-    /**
-     * TODO
-     * @brief performs move on board and removes that move from move array
-     * 
-     * @param {Array[{i,j}]} moves
-     * @param {Array[]} board
-     * 
-     * @returns the move that was removed
-     */
-    doMoveAndPop(moves, board=this.board) {
-        var move = moves.pop();
-        /*if() {
-            this.makeJump(board);
-            return move;
-        }*/
+    isInside(pos, board=this.board) {
+        return (pos.i < board.length && pos.i >= 0 && pos.j >= 0 && pos.j < board[0].length);
     }
 
     /**
@@ -111,7 +215,7 @@ class Game {
         for(var i=0;i<this.size;i++) {
             for(var j=0;j<this.size;j++) {
                 if(board[i][j]==player) {
-                    var pmoves = this.findPossibleMovesFor({i:i,j:j});
+                    var pmoves = this.findPossibleMovesFor({i:i,j:j}, board);
                     if(pmoves.length > 0) {
                         pmoves.forEach((elem)=> elem.piece={i:i,j:j});
                         moves=moves.concat(pmoves);
@@ -129,12 +233,12 @@ class Game {
      * 
      * @returns array containing the possible moves.
      */
-    findPossibleMovesFor(piece) {
+    findPossibleMovesFor(piece, board=this.board) {
 
         var moves = [];
-        var cp = this.board[piece.i][piece.j];
+        var cp = board[piece.i][piece.j];
 
-        var capmoves = this.getCaptureMoves(piece);
+        var capmoves = this.getCaptureMoves(piece, board);
         if(capmoves.length != 0) {
             //moves.push({type:'capture',move:captree});
             moves=moves.concat(capmoves);
@@ -143,8 +247,8 @@ class Game {
         } else if(this.mandatory_capture) {
             for(var i=0;i<this.size;i++)
                 for(var j=0;j<this.size;j++)
-                    if(this.board[i][j] != 0 && cp%2 == this.board[i][j]%2) {
-                        if(this.canCapture({i:i,j:j})) {
+                    if(board[i][j] != 0 && cp%2 == board[i][j]%2) {
+                        if(this.canCapture({i:i,j:j}, board)) {
                             return [];
                         }
                     }
@@ -153,7 +257,7 @@ class Game {
         var i=piece.i, j=piece.j;
         var adjs=[];
         adjs.push({j:j-1},{j:j+1});
-        var pb = this.board[i][j];
+        var pb = board[i][j];
         if(pb == 1) {//the pieces that go from top to bottom
             adjs[0].i = adjs[1].i = i+1;
         } else if(pb==2) //only bottom to top
@@ -164,9 +268,9 @@ class Game {
         }
         
         for(var it=0;it<adjs.length;it++) {
-            if(!this.isInside(adjs[it]))
+            if(!this.isInside(adjs[it], board))
                 continue;
-            var p = this.board[adjs[it].i][adjs[it].j];
+            var p = board[adjs[it].i][adjs[it].j];
             if(p == 0) { // empty
                 moves.push({type:'jump',move:adjs[it]})
             }
@@ -183,8 +287,8 @@ class Game {
      * 
      * @returns if capturing move is possible by `piece` returns true, returns false otherwise.
      */
-    canCapture(piece) {
-        var pobed = this.board[piece.i][piece.j];
+    canCapture(piece, board=this.board) {
+        var pobed = board[piece.i][piece.j];
         if(pobed == 0)
             return false;
         //var opps = Number(!(pobed%2)); //opposite parity
@@ -194,10 +298,10 @@ class Game {
         if(pobed >= 3) adjs = adjs.concat([{i:piece.i-diri,j:adjl.j},{i:piece.i-diri, j:adjr.j}]);
 
         for(var i=0;i<adjs.length;i++) {
-            if(this.isInside(adjs[i]) && this.board[adjs[i].i][adjs[i].j]!=0 
-            && this.board[adjs[i].i][adjs[i].j]%2 != pobed%2) {
+            if(this.isInside(adjs[i],board) && board[adjs[i].i][adjs[i].j]!=0 
+            && board[adjs[i].i][adjs[i].j]%2 != pobed%2) {
                 var jspace = {i: adjs[i].i - ( Math.floor(i/2)*2-1 )*diri, j:adjs[i].j+( (i%2)*2-1 )}; //jump space
-                if(this.isInside(jspace) && this.board[jspace.i][jspace.j]==0)
+                if(this.isInside(jspace, board) && board[jspace.i][jspace.j]==0)
                     return true;
             }
         }
@@ -212,10 +316,10 @@ class Game {
      * 
      * @returns array containing possible capture moves
      */
-    getCaptureMoves(pos, pp = null) {
+    getCaptureMoves(pos, board=this.board, pp = null) {
         var tree = [];
         //var capath = [{type: 'capture', moves:[]}];
-        var cpb = pp||this.board[pos.i][pos.j];
+        var cpb = pp||board[pos.i][pos.j];
         var diri = 1-((cpb-1)%2)*2;
         var adjl = {i:pos.i+diri, j:pos.j -1};
         var adjr = {i:pos.i+diri, j:pos.j +1};
@@ -226,18 +330,18 @@ class Game {
             if(!this.isInside(adjs[i])) {
                 continue;
             }
-            var p = this.board[adjs[i].i][adjs[i].j];
+            var p = board[adjs[i].i][adjs[i].j];
             var adjp = {i:adjs[i].i-(Math.floor(i/2)*2-1)*diri, j: adjs[i].j+( (i%2)*2 -1)};
-            if(p != 0 && p%2 != cpb%2 && this.isInside(adjp) && this.board[adjp.i][adjp.j] == 0) {
-                var oldpiece = this.board[adjs[i].i][adjs[i].j];
-                this.board[adjs[i].i][adjs[i].j] = 0; //temporarily make it empty
+            if(p != 0 && p%2 != cpb%2 && this.isInside(adjp, board) && board[adjp.i][adjp.j] == 0) {
+                var oldpiece = board[adjs[i].i][adjs[i].j];
+                board[adjs[i].i][adjs[i].j] = 0; //temporarily make it empty
                 var killmove = [{tokill:adjs[i], jumpat:adjp}];
-                var nextpaths = this.getCaptureMoves(adjp, cpb);
+                var nextpaths = this.getCaptureMoves(adjp, board, cpb);
                 if(nextpaths.length > 0) {
                     nextpaths.forEach( (el)=> tree.push({type:'capture', moves:killmove.concat(el.moves)}));
                 } else
                     tree.push({type:'capture',moves:killmove});
-                this.board[adjs[i].i][adjs[i].j] = oldpiece; //fill again with original piece
+                board[adjs[i].i][adjs[i].j] = oldpiece; //fill again with original piece
             }
         }
 
@@ -251,20 +355,24 @@ class Game {
      * @param {Object {i,j}} piece Coordinate of a piece that's to be moved
      * @param {Object {i,j}} fpos Coordinate of the final position of piece
      * 
-     * @returns
+     * @returns object with property promoted which is true if the piece was promoted and false otherwise
      */
     makeJump(piece, fpos, board=this.board) {
 
         var pp = board[piece.i][piece.j];
+        var promoted = false;
 
         if(pp <= 2)
         if( (pp == 1 && fpos.i == this.size-1)
         ||  (pp == 2 && fpos.i == 0)) {//reached last row
             pp += 2; //promote to king
+            promoted = true;
         }
 
         board[fpos.i][fpos.j] = pp; //piece moved
         board[piece.i][piece.j] = 0; //set position empty
+
+        return {promoted:promoted};
     }
 
     /**
@@ -290,14 +398,36 @@ class Game {
         return false;
     }
 
+    /**
+     * @brief performs capture move
+     * 
+     * @param {i,j} from 
+     * @param {array} capmove 
+     * @param {Array[][]} board 
+     * 
+     * @returns object containing deadpieces and whether the capturing piece was promoted
+     */
     doCapture(from, capmove, board =this.board) {
+        var deadpieces = [];
         capmove.forEach((el)=>{
+            deadpieces.push(board[el.tokill.i][el.tokill.j]);
             board[el.tokill.i][el.tokill.j]=0;//kill
         });
         //then jump
         var jumpat = capmove[capmove.length-1].jumpat;
-        board[jumpat.i][jumpat.j] = board[from.i][from.j];
-        board[from.i][from.j] = 0;
+        var pp = board[from.i][from.j]; //the piece
+        var promoted = false;
+        /*promote if jumping on last row */
+        if(pp <= 2)
+        if( (pp == 1 && jumpat.i == this.size-1) 
+            || (pp == 2 && jumpat.i == 0)) {
+            pp+=2;
+            promoted = true;
+        }
+        board[jumpat.i][jumpat.j] = pp; //make it jump
+        board[from.i][from.j] = 0; //remove from old pos
+
+        return {deadpieces: deadpieces, promoted:promoted};
     }
 
     /**
@@ -308,7 +438,7 @@ class Game {
      * 
      * @returns the number of pieces captured
      */
-    makeCapture(capmoves, endm) {
+    makeCaptureWithEnd(capmoves, endm) {
 
         for(var i=0;i<capmoves.length;i++) {
             var fpos = capmoves[i].moves.slice(-1)[0].jumpat;
@@ -393,7 +523,7 @@ class Game {
                     var endm = {i:i,j:j};
 
                     if(capmoves) {
-                        var kills = this.makeCapture(capmoves, endm); //attempts capture move
+                        var kills = this.makeCaptureWithEnd(capmoves, endm); //attempts capture move
 
                         if(kills > 0) {
                             this.nextTurn();
